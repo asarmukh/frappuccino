c6a8674fb9c383ad19da20d9d9cf87e9257440e3
tishmal <arturtimur201998@gmail.com>
2025-03-19 01:23:43 +0500

    feat: add order creating its cool and rewrite INFO.txt

--- init.sql
+++ init.sql
@@ -1,6 +1,8 @@
 -- database: :memory:
 DROP TABLE IF EXISTS orders CASCADE;
 
+DROP TABLE IF EXISTS customers CASCADE;
+
 -- Проверяем, существует ли ENUM order_status, чтобы не пытаться создать его дважды
 DO $$ 
 BEGIN
@@ -9,13 +11,18 @@ BEGIN
     END IF;
 END $$;
 
+CREATE TABLE customers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
 CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
-    -- customer_id INT REFERENCES customers(id) ON DELETE SET NULL,
+    customer_id INT REFERENCES customers(id) ON DELETE SET NULL,
     status order_status NOT NULL DEFAULT 'open',
     total_amount DECIMAL(10, 2) NOT NULL CHECK (total_amount >= 0), -- Для денег
     special_instructions TEXT,
-    payment_method VARCHAR(50) NOT NULL,
+    payment_method VARCHAR(50),
     is_completed BOOLEAN NOT NULL DEFAULT FALSE, -- Быстрый фильтр завершённых заказов
     created_at TIMESTAMPTZ DEFAULT NOW(),
     updated_at TIMESTAMPTZ DEFAULT NOW()
