1aafc515efa035c6b418b07912338ad277044e5c
asarmukh <ayansarmukhanovv@gmail.com>
2025-03-27 17:36:48 +0500

    feat: rewrite LoadMenuItems, GetMenuItemByID and DeleteMenuItemByID functions

--- internal/dal/menu_repository.go
+++ internal/dal/menu_repository.go
@@ -3,6 +3,7 @@ package dal
 import (
 	"database/sql"
 	"errors"
+	"fmt"
 	"frappuccino/models"
 	"log"
 	"strings"
@@ -10,8 +11,11 @@ import (
 
 type MenuRepositoryInterface interface {
 	AddMenuItem(menuItem models.MenuItem) (models.MenuItem, error)
-	// LoadMenuItems() ([]models.MenuItem, error)
 	// SaveMenuItems(menuItems []models.MenuItem) error
+	LoadMenuItems() ([]models.MenuItem, error)
+	GetMenuItemByID(id int) (models.MenuItem, error)
+	DeleteMenuItemByID(id int) error
+	UpdateMenu(id int, changeMenu models.MenuItem) (models.MenuItem, error)
 }
 
 type MenuRepository struct {
@@ -41,9 +45,9 @@ func (r MenuRepository) AddMenuItem(menuItem models.MenuItem) (models.MenuItem, 
 	}()
 	categories := "{" + strings.Join(menuItem.Categories, ",") + "}"
 
-	query := `INSERT INTO menu_items (name, description, price, categories) 
-			  VALUES ($1, $2, $3, $4) RETURNING id, created_at, updated_at`
-	err = tx.QueryRow(query, menuItem.Name, menuItem.Description, menuItem.Price, categories).
+	query := `INSERT INTO menu_items (name, description, price, categories, available) 
+			  VALUES ($1, $2, $3, $4, $5) RETURNING id, created_at, updated_at`
+	err = tx.QueryRow(query, menuItem.Name, menuItem.Description, menuItem.Price, categories, menuItem.Available).
 		Scan(&menuItem.ID, &menuItem.CreatedAt, &menuItem.UpdatedAt)
 	if err != nil {
 		return models.MenuItem{}, err
@@ -70,13 +74,177 @@ func (r MenuRepository) AddMenuItem(menuItem models.MenuItem) (models.MenuItem, 
 		}
 	}
 
-	updateQuery := `UPDATE inventory SET quantity = quantity - $1 WHERE id = $2`
-	for _, ingredient := range menuItem.Ingredients {
-		_, err = tx.Exec(updateQuery, ingredient.Quantity, ingredient.IngredientID)
-		if err != nil {
-			return models.MenuItem{}, err
+	return menuItem, nil
+}
+
+// func (r MenuRepository) SaveMenuItems(menuItems []models.MenuItem) error {
+// 	if len(menuItems) == 0 {
+// 		return nil
+// 	}
+
+// 	query := `
+// 		INSERT INTO menu_items (name, description, price, categories, available)
+// 		VALUES ($1, $2, $3, $4, $5)
+// 		`
+// }
+
+func (r MenuRepository) LoadMenuItems() ([]models.MenuItem, error) {
+	var menuItems []models.MenuItem
+
+	query := `SELECT id, name, description, price, categories, available, created_at
+		FROM menu_items`
+
+	rows, err := r.db.Query(query)
+	if err != nil {
+		return nil, fmt.Errorf("ошибка при выполнении запроса для меню: %v", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var menuItem models.MenuItem
+		var categories string
+
+		if err := rows.Scan(&menuItem.ID, &menuItem.Name, &menuItem.Description, &menuItem.Price,
+			&categories, &menuItem.Available, &menuItem.CreatedAt); err != nil {
+			return nil, fmt.Errorf("ошибка при сканировании строки меню: %v", err)
 		}
+
+		if categories != "" {
+			menuItem.Categories = strings.Split(categories, ",")
+		}
+
+		ingredientsQuery := `SELECT ingredient_id, quantity FROM menu_item_ingredients WHERE menu_item_id = $1`
+		ingredientRows, err := r.db.Query(ingredientsQuery, menuItem.ID)
+		if err != nil {
+			return nil, fmt.Errorf("ошибка при выполнении запроса для ингредиентов: %v", err)
+		}
+		defer ingredientRows.Close()
+
+		var ingredients []models.MenuItemIngredient
+		for ingredientRows.Next() {
+			var ingredient models.MenuItemIngredient
+			if err := ingredientRows.Scan(&ingredient.IngredientID, &ingredient.Quantity); err != nil {
+				return nil, fmt.Errorf("ошибка при сканировании ингредиента: %v", err)
+			}
+
+			var inventoryQuantity float64
+			inventoryQuery := `SELECT quantity FROM inventory WHERE id = $1`
+			err := r.db.QueryRow(inventoryQuery, ingredient.IngredientID).Scan(&inventoryQuantity)
+			if err != nil {
+				return nil, fmt.Errorf("ошибка при проверке наличия ингредиента в инвентаре: %v", err)
+			}
+
+			if inventoryQuantity < ingredient.Quantity {
+				return nil, fmt.Errorf("недостаточно ингредиентов %d в инвентаре", ingredient.IngredientID)
+			}
+
+			ingredients = append(ingredients, ingredient)
+		}
+
+		menuItem.Ingredients = ingredients
+		menuItems = append(menuItems, menuItem)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("ошибка при итерации строк меню: %v", err)
+	}
+
+	return menuItems, nil
+}
+
+func (r MenuRepository) GetMenuItemByID(id int) (models.MenuItem, error) {
+	var menuItem models.MenuItem
+	var categories string
+
+	tx, err := r.db.Begin()
+	if err != nil {
+		return models.MenuItem{}, fmt.Errorf("не удалось начать транзакцию: %v", err)
+	}
+	defer tx.Rollback()
+
+	query := `SELECT id, name, description, price, categories, available, created_at, updated_at
+		FROM menu_items WHERE id = $1`
+	err = tx.QueryRow(query, id).Scan(
+		&menuItem.ID,
+		&menuItem.Name,
+		&menuItem.Description,
+		&menuItem.Price,
+		&categories,
+		&menuItem.Available,
+		&menuItem.CreatedAt,
+		&menuItem.UpdatedAt,
+	)
+
+	if categories != "" {
+		menuItem.Categories = strings.Split(categories, ",")
+	}
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return models.MenuItem{}, fmt.Errorf("menu item с ID %d не найден", id)
+		}
+		return models.MenuItem{}, fmt.Errorf("ошибка при получении элемента: %v", err)
+	}
+
+	ingredientsQuery := `SELECT ingredient_id, quantity FROM menu_item_ingredients WHERE menu_item_id = $1`
+	ingredientRows, err := r.db.Query(ingredientsQuery, menuItem.ID)
+	if err != nil {
+		return models.MenuItem{}, fmt.Errorf("ошибка при выполнении запроса для ингредиентов: %v", err)
+	}
+	defer ingredientRows.Close()
+
+	var ingredients []models.MenuItemIngredient
+	for ingredientRows.Next() {
+		var ingredient models.MenuItemIngredient
+		if err := ingredientRows.Scan(&ingredient.IngredientID, &ingredient.Quantity); err != nil {
+			return models.MenuItem{}, fmt.Errorf("ошибка при сканировании ингредиента: %v", err)
+		}
+
+		ingredients = append(ingredients, ingredient)
+	}
+
+	menuItem.Ingredients = ingredients
+
+	if err := ingredientRows.Err(); err != nil {
+		return models.MenuItem{}, fmt.Errorf("ошибка при итерации ингредиентов: %v", err)
+	}
+
+	if err := tx.Commit(); err != nil {
+		return models.MenuItem{}, fmt.Errorf("ошибка при коммите транзакции: %v", err)
 	}
 
 	return menuItem, nil
+}
+
+func (r MenuRepository) DeleteMenuItemByID(id int) error {
+	tx, err := r.db.Begin()
+	if err != nil {
+		return fmt.Errorf("не удалось начать транзакцию: %v", err)
+	}
+	defer tx.Rollback()
+
+	query := `DELETE FROM menu_items WHERE id = $1`
+	res, err := tx.Exec(query, id)
+	if err != nil {
+		return fmt.Errorf("ошибка при удалении элемента: %v", err)
+	}
+
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("не удалось получить количество затронутых строк: %v", err)
+	}
+
+	if rowsAffected == 0 {
+		return fmt.Errorf("inventory item с ID %d не найден", id)
+	}
+
+	if err := tx.Commit(); err != nil {
+		return fmt.Errorf("ошибка при коммите транзакции: %v", err)
+	}
+
+	return nil
+}
+
+func (r MenuRepository) UpdateMenu(id int, changeMenu models.MenuItem) (models.MenuItem, error) {
+	return models.MenuItem{}, nil
 }
