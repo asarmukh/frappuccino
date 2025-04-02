f52825830c8d2e82620bb2de13e9f07d6b9da38a
tishmal <arturtimur201998@gmail.com>
2025-04-03 02:05:39 +0500

    feat: Added healthy wholesale orders

--- models/order.go
+++ models/order.go
@@ -20,3 +20,30 @@ type OrderItem struct {
 	Quantity  float64 `json:"quantity"`
 	Price     float64
 }
+
+type BulkOrderRequest struct {
+	Orders []Order `json:"orders"`
+}
+
+type BulkOrderResponse struct {
+	ProcessedOrders []struct {
+		ID           int     `json:"order_id"`
+		CustomerName string  `json:"customer_name"`
+		Status       string  `json:"status"`
+		TotalAmount  float64 `json:"total,omitempty"`  // Может отсутствовать, если заказ отклонён
+		Reason       string  `json:"reason,omitempty"` // Причина отклонения
+	} `json:"processed_orders"`
+
+	Summary struct {
+		TotalOrders      int     `json:"total_orders"`
+		Accepted         int     `json:"accepted"`
+		Rejected         int     `json:"rejected"`
+		TotalRevenue     float64 `json:"total_revenue"`
+		InventoryUpdates []struct {
+			IngredientID int     `json:"ingredient_id"`
+			Name         string  `json:"name"`
+			QuantityUsed float64 `json:"quantity_used"`
+			Remaining    float64 `json:"remaining"`
+		} `json:"inventory_updates"`
+	} `json:"summary"`
+}
